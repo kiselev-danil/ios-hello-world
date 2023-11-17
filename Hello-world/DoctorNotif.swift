0b2e7365b163b71209055cddd6094f60e66e85e9
Danil Kiselev <dabikis2855@gmail.com>
2023-11-17 17:27:05 +0300

    [+] add divider and padding

--- Hello-world/DoctorNotif.swift
+++ Hello-world/DoctorNotif.swift
@@ -9,24 +9,31 @@ import Foundation
 import SwiftUI
 
 struct DoctorNotif : View {
+    var framePadding :CGFloat = 20
     var body : some View {
-        HStack {
-            Image("doctor photo").clipShape(Circle()).frame(width: 48, height: 48)
-            Spacer()
-            VStack(alignment: .leading, spacing: 16) {
-                Text("Dr.Imran Syahir").font(
-                    Font.custom("Poppins", size: 16)
-                    .weight(.bold)
+        VStack{
+            HStack {
+                Image("doctor photo").resizable().aspectRatio(contentMode:.fit).frame(width: 58).background(Color.white).clipShape(Circle())
+                Spacer()
+                VStack(alignment: .leading, spacing: 16) {
+                    Text("Dr.Imran Syahir").font(
+                        Font.custom("Poppins", size: 16)
+                            .weight(.bold)
                     )
                     .foregroundColor(.white)
-                Text("General Doctor")
-                  .font(Font.custom("Poppins", size: 14))
-                  .foregroundColor(Color(red: 0.8, green: 0.88, blue: 1))
+                    Text("General Doctor")
+                        .font(Font.custom("Poppins", size: 14))
+                        .foregroundColor(Color(red: 0.8, green: 0.88, blue: 1))
+                }
+                Spacer()
+                Image("arrow-right").frame(width: 24, height: 24)
             }
+            
+            Divider().overlay(Color.white.opacity(0.4))
+                .padding(EdgeInsets(top: 16, leading: 0, bottom: 16, trailing: 0))
             Spacer()
-            Image("arrow-right").frame(width: 24, height: 24)
         }
-        .padding(20)
+        .padding(framePadding)
         .frame(width: 327, alignment: .top)
         .background(Color(red: 0.28, green: 0.58, blue: 1))
         .cornerRadius(12)

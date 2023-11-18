2d71037c61c7f5ce51998e1e6958d325d978df23
Danil Kiselev <dabikis2855@gmail.com>
2023-11-18 16:27:46 +0300

    [+] add date and time for doctor card

--- Hello-world/DoctorNotif.swift
+++ Hello-world/DoctorNotif.swift
@@ -9,29 +9,46 @@ import Foundation
 import SwiftUI
 
 struct DoctorNotif : View {
-    var framePadding :CGFloat = 20
-    var body : some View {
-        VStack{
+    var framePadding: CGFloat = 20
+    var doctorName = "Dr. Place Holder"
+    var doctorSpeciality = "General Doctor"
+    var doctorDate = "Sunday, 12 June"
+    var doctorTime = "11:00 - 12:00 AM"
+    
+    
+    var body: some View {
+        VStack {	
             HStack {
-                Image("doctor photo").resizable().aspectRatio(contentMode:.fit).frame(width: 58).background(Color.white).clipShape(Circle())
+                Image("doctor photo").resizable().aspectRatio(contentMode:.fit).frame(width: 48).background(Color.white).clipShape(Circle())
                 Spacer()
                 VStack(alignment: .leading, spacing: 16) {
-                    Text("Dr.Imran Syahir").font(
+                    Text(doctorName).font(
                         Font.custom("Poppins", size: 16)
                             .weight(.bold)
                     )
                     .foregroundColor(.white)
-                    Text("General Doctor")
+                    Text(doctorSpeciality)
                         .font(Font.custom("Poppins", size: 14))
                         .foregroundColor(Color(red: 0.8, green: 0.88, blue: 1))
                 }
                 Spacer()
                 Image("arrow-right").frame(width: 24, height: 24)
-            }
+            }.frame(height: 48)
             
-            Divider().overlay(Color.white.opacity(0.4))
+            Divider().overlay(Color.white.opacity(0.5))
                 .padding(EdgeInsets(top: 16, leading: 0, bottom: 16, trailing: 0))
-            Spacer()
+            HStack {
+                HStack {
+                    Image("calendar-2")
+                    Spacer().frame(width:8)
+                    Text(doctorDate)
+                }.frame(maxWidth: .infinity, alignment: .leading)
+                HStack {
+                    Image("clock")
+                    Spacer().frame(width:8)
+                    Text(doctorTime)
+                }.frame(maxWidth: .infinity, alignment: .trailing)
+            }
         }
         .padding(framePadding)
         .frame(width: 327, alignment: .top)
@@ -42,6 +59,6 @@ struct DoctorNotif : View {
 
 struct DoctorNotif_Previews: PreviewProvider {
     static var previews: some View {
-        DoctorNotif()
+        DoctorNotif(doctorName: "Dr. Place Holder")
     }
 }
